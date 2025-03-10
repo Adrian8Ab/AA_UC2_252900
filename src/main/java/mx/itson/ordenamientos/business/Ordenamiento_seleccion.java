eb84ddf76c30c9d20ed13a89347659af735151dc
ElAdrianb <adrianochoaballesteros@gmail.com>
2025-03-10 16:45:22 -0700

    Se documento la clase Ordenamiento_seleccion y tambien se contaron las operaciones elementales

--- src/main/java/mx/itson/ordenamientos/business/Ordenamiento_seleccion.java
+++ src/main/java/mx/itson/ordenamientos/business/Ordenamiento_seleccion.java
@@ -8,22 +8,27 @@ package mx.itson.ordenamientos.business;
  * @author PC AGRA
  */
 public class Ordenamiento_seleccion {
+    // Método que implementa el algoritmo de ordenamiento por selección
     public static void Ordenamiento_seleccion(int[] lista) {
-       
-        int n = lista.length;
+        int n = lista.length; // (1) Asignación de la longitud del arreglo
         
+        // Recorre el arreglo hasta la penúltima posición
         for (int i = 0; i < n - 1; i++) {
-            int minimo = i;
+            int minimo = i; // (2) Se asume que el mínimo está en la posición i
+            
+            // Busca el menor elemento en la parte no ordenada
             for (int j = i + 1; j < n; j++) {
-                if (lista[j] < lista[minimo]) {
-                    minimo = j;
+                if (lista[j] < lista[minimo]) { // (3) Comparación: verifica si lista[j] es menor que lista[minimo]
+                    minimo = j; // (4) Asignación de un nuevo mínimo si se encuentra un valor menor
                 }
             }
-            // Intercambiar elementos
-            int temp = lista[i];
-            lista[i] = lista[minimo];
-            lista[minimo] = temp;
+            
+            // Intercambiar elementos si se encontró un nuevo mínimo
+            int temp = lista[i]; // (5) Almacena temporalmente el valor de lista[i]
+            lista[i] = lista[minimo]; // (6) Asigna el valor mínimo encontrado a lista[i]
+            lista[minimo] = temp; // (7) Completa el intercambio colocando el valor original en lista[minimo]
+            
+            //Operacionesraciones elemetales = 7
         }
     }
-    
 }
