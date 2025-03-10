5127cb95cee67a2da07ea5c8331139da875a9eea
ElAdrianb <adrianochoaballesteros@gmail.com>
2025-03-10 16:26:29 -0700

    se programo el ordenamiento de seleccion y el main tambien

--- src/main/java/mx/itson/ordenamientos/business/Ordenamiento_seleccion.java
+++ src/main/java/mx/itson/ordenamientos/business/Ordenamiento_seleccion.java
@@ -3,11 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package mx.itson.ordenamientos.business;
-
 /**
  *
  * @author PC AGRA
  */
 public class Ordenamiento_seleccion {
+    public static void Ordenamiento_seleccion(int[] lista) {
+       
+        int n = lista.length;
+        
+        for (int i = 0; i < n - 1; i++) {
+            int minimo = i;
+            for (int j = i + 1; j < n; j++) {
+                if (lista[j] < lista[minimo]) {
+                    minimo = j;
+                }
+            }
+            // Intercambiar elementos
+            int temp = lista[i];
+            lista[i] = lista[minimo];
+            lista[minimo] = temp;
+        }
+    }
     
 }
--- src/main/java/mx/itson/ordenaminetos/main/Main_seleccion.java
+++ src/main/java/mx/itson/ordenaminetos/main/Main_seleccion.java
@@ -4,10 +4,22 @@
  */
 package mx.itson.ordenaminetos.main;
 
+import java.util.Arrays;
+import static mx.itson.ordenamientos.business.Ordenamiento_seleccion.Ordenamiento_seleccion;
+
 /**
  *
  * @author PC AGRA
  */
 public class Main_seleccion {
+      public static void main(String[] args) {
+        int[] lista = {64, 25, 12, 22, 11}; // Se puede cambiar por cualquier otra lista
+        System.out.println("Antes de ordenar: " + Arrays.toString(lista));
+        
+        Ordenamiento_seleccion(lista);
+        
+        System.out.println("Después de ordenar: " + Arrays.toString(lista));
     
 }
+      }
+      
